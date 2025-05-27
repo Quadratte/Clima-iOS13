1f7ec8f31eff2bee8628f64292edcf61e2c1dd62
Sergey Philippov <quadratte@gmail.com>
2025-05-27 17:44:51 +0700

    Add some assets

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -1,24 +1,47 @@
-//
-//  ViewController.swift
-//  Clima
-//
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
-
 import UIKit
 
-class WeatherViewController: UIViewController {
-
-    @IBOutlet weak var conditionImageView: UIImageView!
-    @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var cityLabel: UILabel!
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+  
+  @IBOutlet weak var conditionImageView: UIImageView!
+  @IBOutlet weak var temperatureLabel: UILabel!
+  @IBOutlet weak var cityLabel: UILabel!
+  @IBOutlet var searchTextField: UITextField!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    searchTextField.delegate = self
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    searchTextField.endEditing(true)
+    print(searchTextField.text)
+    return true
+    
+  }
+  
+  @IBAction func searchPressed(_ sender: Any) {
+    print(searchTextField.text!)
+    searchTextField.endEditing(true)
+  }
+  
+  func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+    if searchTextField.text != "" {
+      return true
+    } else {
+      textField.placeholder = "Ты долбоеб!"
+      return false
     }
-
-
+     
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) -> Bool {
+    searchTextField.text = ""
+    return true
+    
+    
+  }
+  
 }
 
